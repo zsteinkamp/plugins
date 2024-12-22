6d1ba03c12cccb86f511ebb8552bc8db7b9eb32b
Zack Steinkamp <zack@steinkamp.us>
2024-12-22 15:43:13 -0800

    email in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import { FC } from "react"
 const Footer: FC<object> = ({ }) => {
   return (
     <div className='not-prose border-t-2 border-tilebg mt-24 pt-4 mb-8 flex'>
-      <div className="text-sm grow">by <Link className="" href="https://steinkamp.us/">Zack Steinkamp</Link></div>
+      <div className="text-sm grow">by <Link className="" href="https://steinkamp.us/">Zack Steinkamp</Link> (<Link href="mailto:zack@steinkamp.us">email</Link>)</div>
       <div className="text-sm"><Link className="" href="https://github.com/zsteinkamp/plugins">Plugins site GitHub Repo</Link></div>
     </div>
   )
