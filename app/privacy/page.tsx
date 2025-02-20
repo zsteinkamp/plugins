2d53ca8594e877439d81079d722aed294b4b2784
Zack Steinkamp <zack@steinkamp.us>
2025-02-20 09:18:24 -0800

    privacy policy

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,32 +1,19 @@
-import Link from "next/link";
-import Footer from "@/components/Footer";
+import Link from 'next/link'
+import Footer from '@/components/Footer'
+import fsp from 'node:fs/promises'
+import ReactMarkdown from 'react-markdown'
 
-export default function Page() {
+export default async function Page() {
+  const rawMarkdown = await fsp.readFile('app/privacy/privacy.md', 'utf-8')
   return (
-    <>
-      <div className="m-auto">
-        <div className="flex mb-12 ml-[-2rem] mt-[-2rem] bg-lcdbg p-8 mr-[-2rem] sm:mr-[-4rem]">
-          <div className="text-xl mt-2 mr-8"><Link href="/">&lt; Home</Link></div>
-          <h1 className="text-5xl text-highlight">
-            Privacy Policy
-          </h1>
-        </div>
+    <div className="prose lg:prose-2xl prose-invert grow mx-auto">
+      <div className="mb-12">
+        <Link href="/">&lt; Home</Link>
       </div>
-      <div className="sm:mx-8">
-        <div className="lg:flex mb-16">
-          <div className="prose lg:prose-2xl prose-invert grow">
-            <div>
-              <p>
-                This website and associated apps do not collect personal data. There is no login, no tracking pixels, no advertising. Downloaded Ableton Livew devices will &quot;phone home&quot; once per session for the purpose of tracking which devices are popular, so that I may invest my time more wisely. The &quot;phone home&quot; packet does not contain any personal data. The IP address of the packet is not retained, only a geo-ip lookup to get the country.
-              </p>
-              <p>
-                Please email zack@steinkamp.us with any questions about this policy.
-              </p>
-            </div>
-            <Footer />
-          </div>
-        </div>
-      </div>
-    </>
+      <ReactMarkdown className="prose-headings:text-highlight">
+        {rawMarkdown}
+      </ReactMarkdown>
+      <Footer />
+    </div>
   )
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,24 @@
+import Link from 'next/link'
+import { FC } from 'react'
 
-import Link from "next/link"
-import { FC } from "react"
-
-const Footer: FC<object> = ({ }) => {
+const Footer: FC<object> = ({}) => {
   return (
-    <div className='not-prose border-t-2 border-tilebg mt-24 pt-4 mb-8 flex'>
-      <div className="text-sm grow">by <Link className="" href="https://steinkamp.us/">Zack Steinkamp</Link> (<Link href="mailto:zack@steinkamp.us">email</Link>)</div>
-      <div className="text-sm"><Link className="" href="https://github.com/zsteinkamp/plugins">Plugins site GitHub Repo</Link></div>
+    <div className="not-prose border-t-2 border-tilebg mt-24 pt-4 mb-8 flex">
+      <div className="text-sm grow">
+        by{' '}
+        <Link className="" href="https://steinkamp.us/">
+          Zack Steinkamp
+        </Link>{' '}
+        (<Link href="mailto:zack@steinkamp.us">email</Link>)
+      </div>
+      <div className="text-sm">
+        <Link className="pr-8" href="/privacy">
+          Privacy Policy
+        </Link>
+        <Link className="" href="https://github.com/zsteinkamp/plugins">
+          Source Code
+        </Link>
+      </div>
     </div>
   )
 }
